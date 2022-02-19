cbac95e171efe11a65c5b70f1aa066e61675e8e3
r0ko <roko.magdalenic7@gmail.com>
2022-02-19 12:27:01 +0100

    initial commit

---
+++ src/App.js
@@ -0,0 +1,104 @@
+import './App.css';
+import React, { Component, Fragment } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Users from './components/users/Users';
+import Search from './components/users/Search';
+import About from './components/pages/About';
+import { Alert } from './components/layout/Alert';
+import axios from 'axios';
+
+class App extends Component {
+  state = {
+    users: [],
+    user: {},
+    loading: false,
+    alert: null,
+  };
+
+  // async componentDidMount() {
+  //   this.setState({ loading: true });
+
+  //   const res = await axios.get(
+  //     `https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_SECRET_ID}`
+  //   );
+
+  //   this.setState({ users: res.data, loading: false });
+  // }
+
+  // Search Github users
+  searchUsers = async (text) => {
+    this.setState({ loading: true });
+
+    const res = await axios.get(
+      `https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_SECRET_ID}`
+    );
+
+    this.setState({ users: res.data.items, loading: false });
+  };
+
+  // Get single Github user
+  getUser = async (username) => {
+    this.setState({ loading: true });
+
+    const res = await axios.get(
+      `https://api.github.com/users/${username}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_SECRET_ID}`
+    );
+
+    this.setState({ users: res.data, loading: false });
+  };
+
+  // Clear users from state
+  clearUsers = () => this.setState({ users: [], loading: false });
+
+  // Set Alert
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg, type } });
+
+    setTimeout(() => this.setState({ alert: null }), 3000);
+  };
+
+  render() {
+    const { users, loading } = this.state;
+
+    return (
+      <Router>
+        <div className='App'>
+          <Navbar />
+          <div className='container'>
+            <Alert alert={this.state.alert} />
+            <Routes>
+              {/* <Route
+                path='/'
+                element={(props) => (
+                  <Fragment>
+                    <Search
+                      searchUsers={this.searchUsers}
+                      clearUsers={this.clearUsers}
+                      showClear={users.length > 0 ? true : false}
+                      setAlert={this.setAlert}
+                    />
+                    <Users loading={loading} users={users} />
+                  </Fragment>
+                )}
+              /> */}
+              <Route path='/about' element={<About />} />
+            </Routes>
+
+            <Fragment>
+              <Search
+                searchUsers={this.searchUsers}
+                clearUsers={this.clearUsers}
+                showClear={users.length > 0 ? true : false}
+                setAlert={this.setAlert}
+              />
+              <Users loading={loading} users={users} />
+            </Fragment>
+          </div>
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;

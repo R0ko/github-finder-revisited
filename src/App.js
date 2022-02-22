c44ea76c1eef142016a3d929298017ce773d6e3f
r0ko <roko.magdalenic7@gmail.com>
2022-02-22 20:28:54 +0100

    adding alert context and not found route

--- src/App.js
+++ src/App.js
@@ -1,47 +1,35 @@
 import './App.css';
-import React, { useState, Fragment } from 'react';
+import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
-import Users from './components/users/Users';
 import User from './components/users/User';
-import Search from './components/users/Search';
 import About from './components/pages/About';
-import { Alert } from './components/layout/Alert';
+import Home from './components/pages/Home';
+import Alert from './components/layout/Alert';
+import NotFound from './components/pages/NotFound';
+
 import GithubState from './context/github/GithubState';
+import AlertState from './context/alert/AlertState';
 
 const App = () => {
-  const [alert, setAlert] = useState(null);
-
-  // Set Alert
-  const showAlert = (msg, type) => {
-    setAlert({ msg, type });
-
-    setTimeout(() => setAlert(null), 3000);
-  };
-
   return (
     <GithubState>
-      <Router>
-        <div className='App'>
-          <Navbar />
-          <div className='container'>
-            <Alert alert={alert} />
-            <Routes>
-              <Route
-                path='/'
-                element={
-                  <Fragment>
-                    <Search setAlert={showAlert} />
-                    <Users />
-                  </Fragment>
-                }
-              />
-              <Route path='/about' element={<About />} />
-              <Route path='/user/:login' element={<User />} />
-            </Routes>
+      <AlertState>
+        <Router>
+          <div className='App'>
+            <Navbar />
+            <div className='container'>
+              <Alert />
+              <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/about' element={<About />} />
+                <Route path='/user/:login' element={<User />} />
+                <Route path='*' element={<NotFound />} />
+              </Routes>
+            </div>
           </div>
-        </div>
-      </Router>
+        </Router>
+      </AlertState>
     </GithubState>
   );
 };
